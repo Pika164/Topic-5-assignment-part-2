df9cb76ac5b69a23e1ef913f10b25e58d75812a8
Linkin Krysa <339742660@GoTVDSB.ca>
2025-02-24 14:40:31 -0500

    Add project files.

---
+++ Topic 5 assignment part 2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_5_assignment_part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

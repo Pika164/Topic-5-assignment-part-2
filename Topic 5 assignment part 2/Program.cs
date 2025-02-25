8e8547204aa27a6267b7e0c20d0e1f4dd81d0919
Linkin Krysa <339742660@GoTVDSB.ca>
2025-02-25 13:39:26 -0500

    Finished part 2. Onto part 3.

--- Topic 5 assignment part 2/Program.cs
+++ Topic 5 assignment part 2/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int userGuess, secretNumber;
+
+            secretNumber = 5;
+
+            Console.WriteLine("Hello and welcome to my dumb guessing game:). You will get one guess to guess the secret number.");
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to continue");
+            Console.WriteLine();
+            Console.Write("Now then, What is your guess on my secret number (1-10)? You'll get a special prize for getting it right. ");
+            userGuess = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            if (userGuess == 5)
+            {
+                Console.WriteLine("You are Correct! Your prize is getting it correct so horray!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry but you got it wrong. You were close though!");
+            }
         }
     }
 }
